68c33ecd4b1b6bd455561dcecf13ed9d097dc142
pedropmedina <pedrodelilia@gmail.com>
2018-09-02 23:29:53 -0400

    Cleaned up server to avoid calling authenticateUser repeatedly

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
-const { GraphQLServer } = require('graphql-yoga');
 const fs = require('fs');
+const opn = require('opn');
+const path = require('path');
+const { google } = require('googleapis');
+const { GraphQLServer } = require('graphql-yoga');
 
-// oAuth2 instance from user.resolver
-const { oAuth2Client } = require('./api/user/user.resolvers');
+const keys = require('./keys.json');
 
 // graphQL configuration from api/index.js
 const graphQLConfig = require('./api');
@@ -17,68 +19,48 @@ const options = {
 // instance of graphql graphql-yoga server
 const server = new GraphQLServer(graphQLConfig);
 
-// Run express middeware to on /auth to extract code and access token
-// This is only going to run upon user's signin
-server.express.use('/auth', async (req, res, next) => {
-	const code = req.query.code;
-	const { tokens } = await oAuth2Client.getToken(code);
-	oAuth2Client.setCredentials(tokens);
+const authenticateUser = () => {
+	// instantiate OAuth2
+	const oAuth2Client = new google.auth.OAuth2(
+		keys.web.client_id,
+		keys.web.client_secret,
+		keys.web.redirect_uris[0],
+	);
 
-	// set Authorization on context for access throughout app
-	// server.context = { Authorization: tokens };
-	fs.writeFile('tokens.json', JSON.stringify(tokens), err => {
-		if (err) throw err;
-		console.log('tokens were saved!');
+	// generate authorization url for granting permission
+	const authorizeUrl = oAuth2Client.generateAuthUrl({
+		access_type: 'offline',
+		// scope can be a string or array in case of more than one
+		scope: 'https://www.googleapis.com/auth/books',
 	});
 
-	next();
-});
+	// for local testing purpose, open permission url on browser.
+	// To be handle from Front-end once I develop application
+	opn(authorizeUrl);
+
+	// Run express middeware to on /auth to extract code and access token
+	// This is only going to run upon user's signin
+	server.express.use('/auth', async (req, res, next) => {
+		const code = req.query.code;
+		const { tokens } = await oAuth2Client.getToken(code);
+		oAuth2Client.setCredentials(tokens);
+
+		// set Authorization on context for access throughout app
+		// server.context = { Authorization: tokens };
+		fs.writeFile('tokens.json', JSON.stringify(tokens), err => {
+			if (err) throw err;
+			console.log('tokens were saved!');
+		});
+
+		next();
+	});
+};
 
 server.start(options, ({ port }) => {
 	console.log(`🚀 Server is up on port ${port}`);
+
+	fs.readFile(path.join(__dirname, '../tokens.json'), 'utf8', (err, data) => {
+		if (err) throw err;
+		if (!JSON.parse(data)) authenticateUser();
+	});
 });
-
-// References:
-// https://medium.com/@pablo127/google-api-authentication-with-oauth-2-on-the-example-of-gmail-a103c897fd98
-
-// https://github.com/google/google-auth-library-nodejs
-
-// // Google Oauth2 ------------------- Start
-// const getAuthenticatedClient = () => {
-// 	return new Promise((resolve, reject) => {
-// 		const oAuth2Client = new OAuth2Client(
-// 			keys.web.client_id,
-// 			keys.web.client_secret,
-// 			keys.web.redirect_uris[0],
-// 		);
-
-// 		const authorizeUrl = oAuth2Client.generateAuthUrl({
-// 			access_type: 'offline',
-// 			scope: 'https://www.googleapis.com/auth/books',
-// 		});
-
-// 		opn(authorizeUrl);
-
-// 		server.express.use('/auth', async (req, res, next) => {
-// 			const code = req.query.code;
-
-// 			// With the presence of the code we go ahead and obtain the token
-// 			const { tokens } = await oAuth2Client.getToken(code);
-// 			oAuth2Client.setCredentials(tokens);
-// 			resolve(oAuth2Client);
-// 			next();
-// 		});
-// 	});
-// };
-
-// const main = async () => {
-// 	try {
-// 		const oAuth2Client = await getAuthenticatedClient();
-// 		const url = 'https://www.googleapis.com/books/v1/mylibrary/bookshelves';
-// 		const res = await oAuth2Client.request({ url });
-// 		console.log(res.data);
-// 	} catch (error) {
-// 		console.log('error => ', error);
-// 	}
-// };
-// // Google Oauth2 ------------------- End
