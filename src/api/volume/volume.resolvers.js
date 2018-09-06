7c718b23aff82a11ea8940d19ded65de4c573dc6
pedropmedina <pedrodelilia@gmail.com>
2018-09-06 16:01:21 -0400

    Added Volume type and resolver to getVolumens in selected bookshelf

--- src/api/volume/volume.resolvers.js
+++ src/api/volume/volume.resolvers.js
@@ -6,7 +6,8 @@ const getVolumes = async (root, args, ctx, info) => {
 	const res = await books.mylibrary.bookshelves.volumes.list({
 		shelf: args.shelfId,
 	});
-	console.log(res.data);
+	console.log(res.data.items);
+	return res.data.items;
 };
 
 module.exports = {
