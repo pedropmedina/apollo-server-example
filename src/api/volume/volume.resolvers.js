964e9adf133ef43f4964972089d0b2e719339b68
pedropmedina <pedrodelilia@gmail.com>
2018-09-07 00:00:38 -0400

    Added options search add remove volume

--- src/api/volume/volume.resolvers.js
+++ src/api/volume/volume.resolvers.js
@@ -10,8 +10,38 @@ const getVolumes = async (root, args, ctx, info) => {
 	return res.data.items;
 };
 
+const searchVolume = async (root, args, ctx, info) => {
+	const res = await books.volumes.list({
+		q: args.query,
+		orderBy: 'relevance',
+	});
+	console.log(res.data.items[0]);
+	return res.data.items;
+};
+
+const addVolume = async (root, { input }, ctx, info) => {
+	const res = await books.mylibrary.bookshelves.addVolume({
+		shelf: input.shelf,
+		volumeId: input.volumeId,
+	});
+	return `volume with id ${input.volumeId} was added.`;
+};
+
+const removeVolume = async (root, { input }, ctx, info) => {
+	const res = await books.mylibrary.bookshelves.removeVolume({
+		shelf: input.shelf,
+		volumeId: input.volumeId,
+	});
+	return `volume with id ${input.volumeId} was removed.`;
+};
+
 module.exports = {
 	Query: {
 		getVolumes,
+		searchVolume,
+	},
+	Mutation: {
+		addVolume,
+		removeVolume,
 	},
 };
