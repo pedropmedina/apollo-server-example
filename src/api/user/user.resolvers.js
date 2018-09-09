bbcbcdb2ec15c88643855843d887e491310892f7
pedropmedina <pedrodelilia@gmail.com>
2018-09-09 12:07:59 -0400

    Added signIn resolver

--- src/api/user/user.resolvers.js
+++ src/api/user/user.resolvers.js
@@ -6,21 +6,46 @@ const User = require('./user.model');
 
 const oauth2 = google.oauth2('v2');
 
-const signin = async (root, args, ctx, info) => {
-	// const userInfo = await oauth2.userinfo.get();
-	// console.log(userInfo.data);
-
-	// console.log(args.idToken);
-
-	const ticket = await oAuth2Client.verifyIdToken({
+const signIn = async (root, args, ctx, info) => {
+	// Verifies id_token from signed in user with following verifications:
+	// 1. The id_token is properly signed by Google
+	// 2. The value of the aud equals one of app's client IDs
+	// 3. The value of iss equals accounts.google.com
+	// 4. Check the exp of token has not passed.
+	const verification = await oAuth2Client.verifyIdToken({
 		idToken: args.idToken,
 	});
-	const payload = ticket.getPayload();
-	console.log(payload);
+	const payload = verification.getPayload();
+
+	const { sub, name, given_name, family_name, email, picture } = payload;
+
+	const user = {
+		userId: sub,
+		name,
+		givenName: given_name,
+		familyName: family_name,
+		email,
+		picture,
+	};
+
+	try {
+		const foundUser = await User.findOne({ userId: user.userId });
+
+		if (foundUser) {
+			console.log('user found => ', foundUser);
+			return;
+		} else {
+			const createdUser = await User.create(user);
+			console.log(createdUser);
+			return;
+		}
+	} catch (error) {
+		console.log(error.message);
+	}
 };
 
 module.exports = {
 	Mutation: {
-		signin,
+		signIn,
 	},
 };
