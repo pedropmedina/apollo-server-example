fc69b23dfa44ed04eb9932dc57c36b50b2ee5b90
pedropmedina <pedrodelilia@gmail.com>
2018-09-10 17:29:40 -0400

    Implemented all CRUD operations under CRUD file

--- src/api/CRUD.js
+++ src/api/CRUD.js
@@ -1,7 +1,50 @@
 const createOne = async (model, input) => {
-	return await model.create(input).exec();
+	try {
+		return await model.create(input).exec();
+	} catch (error) {
+		console.error(error.message);
+	}
+};
+
+const readOne = async (model, id) => {
+	try {
+		return await mode.findById(id).exec();
+	} catch (error) {
+		console.error(error.message);
+	}
+};
+
+const readMany = async model => {
+	try {
+		return await model.find({}).exec();
+	} catch (error) {
+		console.error(error.message);
+	}
+};
+
+const updateOne = async (model, input) => {
+	try {
+		const { id, ...update } = input;
+		return await model
+			.findByIdAndUpdate(id, { $set: update }, { new: true })
+			.exec();
+	} catch (error) {
+		console.error(error.message);
+	}
+};
+
+const deleteOne = async (model, id) => {
+	try {
+		return await model.findByIdAndRemove(id).exec();
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 module.exports = {
 	createOne,
+	readOne,
+	readMany,
+	updateOne,
+	deleteOne,
 };
